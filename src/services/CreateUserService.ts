bfe33365a2e95f3954340cb4b4b83d88ce56e2af
egfs1 <eduardogomesfs4@gmail.com>
2022-04-25 02:05:30 -0300

    aula 2

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,25 @@
+import prismaClient from "../prisma"
+
+interface IUserRequest {
+    name: string,
+    email: string,
+    admin?: boolean
+}
+
+class CreateUserService {
+    async execute({name, email, admin}: IUserRequest) {
+        try {
+            return prismaClient.users.create({
+                data: {
+                    name: name,
+                    email: email,
+                    admin: admin
+                },
+            })
+        } catch (e) {
+            throw new Error("Email already exists")
+        }
+    }
+}
+
+export {CreateUserService}
