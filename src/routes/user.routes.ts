2ddd53824327e635b37464e7da05941a701d7a66
egfs1 <eduardogomesfs4@gmail.com>
2022-04-25 23:26:39 -0300

    removing middlewares to create user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,7 +8,7 @@ const router = Router()
 const userController = new UserController()
 
 router.get('/', ensureAuthenticated, userController.index)
-router.post('/save',  ensureAuthenticated, ensureAdmin, userController.create)
+router.post('/save', userController.create)
 router.post('/authenticate', userController.authenticate)
 router.get('/compliments/send', ensureAuthenticated, userController.listUserSendCompliments)
 router.get('/compliments/receive', ensureAuthenticated, userController.listUserReceiveCompliments)
